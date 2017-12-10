d97bd762643358871cfff63842b6fe338cc82944
leeshlay <kijak.shia@gmail.com>
2017-12-10 17:25:21 +0100

    Add database connection

--- api-gateway/src/main/java/agh/iosr/controller/UploadController.java
+++ api-gateway/src/main/java/agh/iosr/controller/UploadController.java
@@ -1,32 +1,21 @@
 package agh.iosr.controller;
 
-import agh.iosr.event.api.EventSender;
-import agh.iosr.event.model.EventMessage;
-import agh.iosr.model.VideoConversionType;
-import agh.iosr.storage.services.S3StorageService;
+import agh.iosr.storage.services.UploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.net.URL;
 
 @RestController
 @RequestMapping("upload")
 public class UploadController {
 
     @Autowired
-    private S3StorageService s3StorageService;
-
-    @Autowired
-    private EventSender eventSender;
+    private UploadService uploadService;
 
     @PostMapping
-    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile uploadedFile){
-        URL fileUrl = s3StorageService.uploadFile(uploadedFile.getOriginalFilename(), uploadedFile);
-        //put message to queue
-        eventSender.sendEvent(new EventMessage(fileUrl.toString(), VideoConversionType.TWO_TIMES_FASTER));
-        //put to db
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile uploadedFile, @RequestParam("userId") String userId){
+        uploadService.uploadFile(userId, uploadedFile);
         return ResponseEntity.ok().build();
     }
 
---
+++ api-gateway/src/main/java/agh/iosr/storage/services/UploadService.java
@@ -0,0 +1,37 @@
+package agh.iosr.storage.services;
+
+import agh.iosr.event.api.EventSender;
+import agh.iosr.event.model.EventMessage;
+import agh.iosr.model.VideoConversionType;
+import agh.iosr.model.VideoData;
+import agh.iosr.repository.VideoDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.net.URL;
+
+@Service
+public class UploadService {
+
+    @Autowired
+    private S3StorageService s3StorageService;
+
+    @Autowired
+    private EventSender eventSender;
+
+    @Autowired
+    private VideoDataRepository videoDataRepository;
+
+    @Transactional
+    public void uploadFile(String userId, MultipartFile uploadedFile) {
+        VideoConversionType videoConversionType = VideoConversionType.TWO_TIMES_FASTER;
+        URL fileUrl = s3StorageService.uploadFile(userId +"/"+uploadedFile.getOriginalFilename(), uploadedFile);
+
+        eventSender.sendEvent(new EventMessage(fileUrl.toString(), videoConversionType));
+
+        VideoData data = new VideoData(userId, uploadedFile.getOriginalFilename(), fileUrl.toString(), videoConversionType);
+        videoDataRepository.save(data);
+    }
+}
